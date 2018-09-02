7afea3233366331d4478206f865ef68301dc11c2
Pavel Bezdornov <pavel.alpinweb@yandex.ru>
2018-09-02 15:02:15 +0600

    add dnd htnl5

--- src/friends.js
+++ src/friends.js
@@ -5,3 +5,38 @@ import './styles/styles.scss';
 // container.innerHTML = html;
 
 
+const source = document.querySelector('.friends-catalog__list--friends');
+const target = document.querySelector('.friends-catalog__list--filter');
+
+makeDnD([source, target]);
+
+function makeDnD(zones) {
+  let currentDrag;
+
+  zones.forEach(zone => {
+      zone.addEventListener('dragstart', (e) => {
+          currentDrag = { source: zone, node: e.target };
+      });
+
+      zone.addEventListener('dragover', (e) => {
+          e.preventDefault();
+
+      });
+
+      zone.addEventListener('drop', (e) => {
+          if (currentDrag) {
+              e.preventDefault();
+              currentDrag.node.style.position = 'static';
+              if (currentDrag.source !== zone) {
+                  if (e.target.classList.contains('friends-card')) {
+                      zone.insertBefore(currentDrag.node, e.target.nextElementSibling);
+                  } else {
+                      zone.insertBefore(currentDrag.node, zone.lastElementChild);
+                  }
+              }
+
+              currentDrag = null;
+          }
+      });
+  })
+}
